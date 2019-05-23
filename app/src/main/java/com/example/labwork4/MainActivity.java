7317701b7cb0b4f6adef9a24d58842e2f0da0a67
Tragen47 <Tragen.2012@mail.ru>
2019-05-23 23:26:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/labwork4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.labwork4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        finish();
+    }
+}
